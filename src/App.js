7f2f5b68b17034be301169798cb679880356d8c4
LeraKravts <60606888+LeraKravts@users.noreply.github.com>
2024-02-17 13:53:55 +0100

    project MVP is ready

--- src/App.js
+++ src/App.js
@@ -6,20 +6,24 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header>MY TRAVEL JOURNAL</header>
+      <header>
+        <img src="./planet.png" />
+        <h1>my travel journal.</h1>
+        </header>
       {data.map(
         function(item){
           console.log(item , '<<<<item')
           return (
             <Card 
             id ={item.id}
-            // {...item}
-            imageUrl ={item.imageUrl}
-            location ={item.location}
-            googleMapsUrl = {item.googleMapsUrl}
-            startDate = {item.startDate}
-            endDate = {item.endDate}
-            description = {item.description}
+            {...item}
+            // imageUrl ={item.imageUrl}
+            // location ={item.location}
+            // googleMapsUrl = {item.googleMapsUrl}
+            // startDate = {item.startDate}
+            // endDate = {item.endDate}
+            // description = {item.description}
+            // item = {item}
           
             />
 
